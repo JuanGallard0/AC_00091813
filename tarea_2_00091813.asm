daacde5fc395ea4819bb3236e633f5762ba156aa
Juan Gallardo <00091813@uca.edu.sv>
2020-04-24 02:23:52 -0600

    Add: ex 03

--- tarea_2_00091813.asm
+++ tarea_2_00091813.asm
@@ -2,8 +2,8 @@
 
 	;Ejercicio 1.
 
-	;Ultimos 5 digitos de mi carnet: 91813
-	;Promedio = (9+1+8+1+3)/5 = 4.4 ~= 4
+;Ultimos 5 digitos de mi carnet: 91813
+;Promedio = (9+1+8+1+3)/5 = 4.4 ~= 4
 
 	mov	WORD[200h], "M"
 	mov	WORD[201h], "e"
@@ -16,31 +16,65 @@
 	mov	WORD[209h], "r"
 	mov	WORD[20Ah], "o"
 
+
 	;Ejercicio 2
 
-	mov	ax, 0000h
-	mov	bx, 0000h
-	mov	cx, 0000h
-	mov	dx, 0000h
+	mov	ax, 0000h	;enfermos
+	mov	bx, 0000h	;indice
+	mov 	si, 0000h	;multiplo
 
 	mov 	al, 2d	;numero inicial de enfermos
-	mov	bl, 0d	;indice
-	mov	cl, 2d	;multiplo a utilizar en la estimacion
+	mov	si, 2d	;multiplo a utilizar en cada estimacion
 
-bucle:	mul 	cx	;estimacion
+loop1:	mul 	si	;estimacion
 	mov	[210h+bx], ax	;guardar estimacion
-	inc 	bl 	;incrementar indice
+	inc 	bl 	;indice++
 
-	cmp	bl, 16d	;condicion del bucle
-	jne	bucle	;seguir el bucle si el indice no es 16
+	cmp	bl, 6d	;seguir bucle mientras indice sea inferior a 6d
+	jb	loop1	
+
+;las estimaciones se pasan de 255 a partir de la septima
 	
-	;a partir de los 100h enfermos el output del dump ya no los muestra
-	;la 16ava estimacion es 10000h por lo que AX desborda y se guarda 1 en DX
+loop2:	mul 	si	
+	mov	[210h+bx], ah
+	inc 	bl
+	mov	[210h+bx], al	
+	inc 	bl
+
+	cmp	bl, 16d	
+	jb	loop2	
 
 
 	;Ejercicio 3
 
-	
+	mov	ax, 0000h	;variable temporal
+	mov	bx, 0000h	;indice
+	mov	cx, 0000h	;F[n-2]
+	mov	dx, 0000h	;F[n-1]
+
+	mov	bl, 2d	;indice = 2
+	mov 	dl, 1d 	;F[1] = 1
+	mov	[220h], cl	;guardar F[0]
+	mov	[221h], dl	;guardar F[1]
+
+loop3:	mov	ax, dx	;AX = F[n-1]
+	add	ax, cx	;AX += F[n-2]
+	mov	[220h+bx], ax 	;guardar AX
+	mov	cx, dx	;F[n-2] = F[n-1]
+	mov	dx, ax 	;F[n-1] = AX
+	inc 	bl 	;indice++
+
+	cmp	bl, 14d	;seguir bucle mientras indice sea inferior a 14d
+	jb	loop3	
+
+;solo el 16avo termino se pasa de 255 
+
+	mov	ax, dx	
+	add	ax, cx	
+	mov	[220h+bx], ah
+	inc 	bl 	
+	mov	[220h+bx], al
+	mov	cx, dx	
+	mov	dx, ax 	
 
 	int 20h
-
